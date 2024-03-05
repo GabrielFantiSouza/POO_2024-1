d363411323568f900eb381fabd584e4f8c21b6f5
GabrielFantiSouza <161329967+GabrielFantiSouza@users.noreply.github.com>
2024-03-05 23:50:39 +0000

    Exercicio modelagem de retangulo

--- ExercicioRetangulo/src/App.java
+++ ExercicioRetangulo/src/App.java
@@ -1,6 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        Menu menu=new menu;
-        menu();
+    public static void main(String[] args){
+        Retangulo retangulo = new Retangulo();
+        retangulo.menu();
     }
 }
--- ExercicioRetangulo/src/Retangulo.java
+++ ExercicioRetangulo/src/Retangulo.java
@@ -5,37 +5,58 @@ public class Retangulo{
     private double lado1;
     private double lado2;
 
-    public Rentangulo(double lado1, double lado2){
-        this.lado1=lado1;
-        this.lado2=lado2;
-    }
-    public Quadrado(double ladoigual){
-        this.ladoigual=ladoigual;
+    public Retangulo(){
+
     }
 
-    public double areaRetangulo(double lado1, double lado2){
+    public Retangulo(double lado1, double lado2){ //construtor do retangulo
+        this.lado1=lado1;
+        this.lado2=lado2;
+        }
+
+    public Retangulo(double lado1){ //construtor do quadrado
+        this.lado1 = lado1;
+    }
+
+    public double areaRetangulo(double lado1, double lado2){//calcula a area
         return lado1*lado2;
     }
 
-    public double perimetroRetangulo(double lado1, double lado2){
+    public double areaQuadrado(double lado1){//calcula a area
+        return lado1*lado1;
+    }
+
+    public double perimetroRetangulo(double lado1, double lado2){//calcula o perimetro
         return (lado1*2)+(lado2*2);
     }
 
-    public getLado1(){
-        return this.lado1;
+    public double perimetroQuadrado(double lado1){//calcula o perimetro
+        return lado1*4;
     }
 
-    public getlado2(){
-        return this.lado2;
+    public double getLado1(){//fornece o lado 1
+        return lado1;
     }
 
-    public menu(){
-        Scanner in=new Scanner(Sysmte.in);
+    public double getlado2(){//fornece o lado 2
+        return lado2;
+    }
+
+    public void menu(){//menu de inicialização do app
+        Scanner in=new Scanner(System.in);
         System.out.println("Digite o primeiro lado do retangulo:");
         double lado1=in.nextDouble();
         System.out.println("Digite o segundo lado do retangulo:");
         double lado2=in.nextDouble();
-        Retangulo retangulo = new Retangulo(lado1,lado2);
-        System.out.println();
+        Retangulo retangulo;
+        retangulo = new Retangulo(lado1,lado2); 
+            if(retangulo.lado1==retangulo.lado2){
+                retangulo = new Retangulo(lado1);
+                System.out.println("Você criou um quadrado de lado "+retangulo.getLado1());
+                System.out.println("Sua área é "+areaQuadrado(lado1)+" e seu perímetro é "+perimetroQuadrado(lado1));
+            }else{
+                System.out.println("Você criou um retangulo de lados "+retangulo.getLado1()+" e "+retangulo.getlado2());
+                System.out.println("Sua área é "+areaRetangulo(lado1, lado2)+" e seu perímetro é "+perimetroRetangulo(lado1, lado2));
+            }
     }
 }
